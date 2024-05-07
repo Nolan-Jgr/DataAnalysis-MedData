ab3c641c302636d39a7eb26997c9c2a6d7997b7f
Nolan-Jgr <nolan.jaeger2000@gmail.com>
2024-05-07 15:51:45 -0400

    completed draw_cat_plot func

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -4,30 +4,28 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # 1
-df = None
+df = pd.read_csv("./medical_examination.csv")
 
 # 2
-df['overweight'] = None
+df['overweight'] = np.where(df["weight"] / ((df["height"]/100) * (df["height"]/100)) > 25,1,0)
 
 # 3
-
+df["cholesterol"] = np.where(df["cholesterol"] == 1,0,1)
+df["gluc"] = np.where(df["gluc"] == 1,0,1)
 
 # 4
 def draw_cat_plot():
     # 5
-    df_cat = None
-
+    df_cat = pd.melt(df,id_vars=["cardio"],value_vars=["cholesterol", "gluc", "smoke", "alco", "active", "overweight"])
 
     # 6
-    df_cat = None
+    df_cat = df_cat.groupby(["cardio", "variable", "value"]).size().reset_index()
+    df_cat = df_cat.rename(columns={0: "total"})
+    #print(df_cat)
     
-
-    # 7
-
-
-
     # 8
-    fig = None
+    fig = sns.catplot(data=df_cat, x="variable", y="total", hue="value", col="cardio", kind="bar")
+
 
 
     # 9
